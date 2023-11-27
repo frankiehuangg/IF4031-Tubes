fc94231f5af7f8cea8573c7660283389bb83c75a
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-11-28 00:55:04 +0700

    feat: create event API endpoint

--- ticket-app/src/routers/Router.go
+++ ticket-app/src/routers/Router.go
@@ -11,7 +11,9 @@ func SetupRoutes() *mux.Router {
 	// Initialize the mux router
 	router := mux.NewRouter()
 
+	// List of routes
 	router.HandleFunc("/events", services.GetEvents).Methods("GET")
+	router.HandleFunc("/events", services.CreateEvent).Methods("POST")
 	router.HandleFunc("/events/{event_id}", services.GetEventDetail).Methods("GET")
 
 	fmt.Printf("Server started at %s", os.Getenv("PORT"))
---
+++ ticket-app/src/services/CreateEventService.go
@@ -0,0 +1,80 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"microservices/ticket/src/clients"
+	"microservices/ticket/src/models"
+	"net/http"
+)
+
+func CreateEvent(w http.ResponseWriter, r *http.Request) {
+	eventName := r.FormValue("event_name")
+	eventDate := r.FormValue("event_date")
+	totalSeat := r.FormValue("total_seat")
+
+	var response = models.EventJSONResponse{}
+
+	if eventName == "" {
+		response = models.EventJSONResponse{
+			Type:    "error",
+			Message: "event name form value is missing!",
+		}
+	} else if eventDate == "" {
+		response = models.EventJSONResponse{
+			Type:    "error",
+			Message: "event date form value is missing!",
+		}
+	} else if totalSeat == "" {
+		response = models.EventJSONResponse{
+			Type:    "error",
+			Message: "total seat form value is missing!",
+		}
+	} else {
+		db := clients.GetDBInstance()
+
+		fmt.Printf("[!] [POST] [/events]\n")
+
+		var retrievedEventId int
+		var retrievedEventName string
+		var retrievedEventDate string
+		var retrievedTotalSeat int
+
+		var events []models.Events
+
+		err := db.QueryRow(
+			"INSERT INTO events (event_name, event_date, total_seat) VALUES ($1, $2, $3) RETURNING * ;",
+			eventName,
+			eventDate,
+			totalSeat,
+		).Scan(
+			&retrievedEventId,
+			&retrievedEventName,
+			&retrievedEventDate,
+			&retrievedTotalSeat,
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		events = append(events, models.Events{
+			EventID:   retrievedEventId,
+			EventName: retrievedEventName,
+			EventDate: retrievedEventDate,
+			TotalSeat: retrievedTotalSeat,
+		})
+
+		response = models.EventJSONResponse{
+			Type:    "success",
+			Data:    events,
+			Message: "Event has been created successfully",
+		}
+	}
+
+	err := json.NewEncoder(w).Encode(response)
+
+	if err != nil {
+		panic(err)
+	}
+}
